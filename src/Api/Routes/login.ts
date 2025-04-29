77431a5fbf2fdeae35951bc40772c7b7a7d12086
João Rafael <joaorafael1344@gmail.com>
2025-04-29 09:34:00 -0300

    Re-envio de login feito

--- src/Api/Routes/login.ts
+++ src/Api/Routes/login.ts
@@ -1,15 +1,37 @@
+import { PrismaClient } from "../../generated/prisma";
 import { app } from "../../server";
+import { RequestBody } from "../Interface/request-body";
+import { verifyPassword } from "../Helpers/hash-password";
 
-export async function login () {
-
-    app.post('/login', (request, reply) =>  {
+const prisma = new PrismaClient();
 
 
+export function login() {
+
+    app.post('/login', async (request, reply) =>  {
         app.log.info("User accessed login route");
-
-
-    });
     
-
+        const { email, password } = request.body as RequestBody;
+    
+        const user = await prisma.users.findFirst({
+            select: {
+                password: true
+            },
+            where: { email: email }
+        });
+    
+        if (!user) {
+            return reply.status(401).send({ message: 'Invalid email or password' });
+        }
+    
+        const isPasswordCorrect = await verifyPassword(password, user.password);
+    
+        if (!isPasswordCorrect) {
+            return reply.status(401).send({ message: 'Invalid email or password' });
+        }
+    
+    
+        reply.send({ message: 'Login successful' });
+    });
 
 }
